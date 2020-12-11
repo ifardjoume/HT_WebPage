d8754b2d7d8b73ab55c571b4be10b5e7ae47af32
GonzaLifrieri <gonza.lifrieri@gmail.com>
2020-12-11 16:34:58 -0300

    Footer created

--- src/components/footer/Footer.js
+++ src/components/footer/Footer.js
@@ -1,9 +1,31 @@
-import React from 'react'
+import { Button } from '../../globalStyles';
+import React from 'react';
+import {
+    FaFacebook,
+    FaInstagram,
+    FaYoutube,
+    FaTwitter,
+    FaLinkedin
+  } from 'react-icons/fa';
 import {
     FooterContainer,
     FooterSubscription,
     FooterSubText,
-    FooterSubHeading
+    FooterSubHeading,
+    Form,
+    FormInput,
+    FooterLinksContainer,
+    FooterLinksWrapper,
+    FooterLinkItems,
+    FooterLinkTitle,
+    FooterLink,
+    SocialMedia,
+    SocialMediaWrap,
+    SocialLogo,
+    SocialIcon,
+    WebsiteRights,
+    SocialIcons,
+    SocialIconLink
   } from './Footer.elements';
   
 
@@ -18,7 +40,61 @@ const Footer = () => {
                 <FooterSubText>
                     We are just getting started!
                 </FooterSubText>
+                <Form>
+                 <FormInput name= "email" type="email" placeholder="Your Email"/>
+                     <Button fontBig>Send</Button>
+                </Form>
             </FooterSubscription>
+            <FooterLinksContainer>
+                <FooterLinksWrapper>
+                    <FooterLinkItems>
+                        {/* //Falta agregar rutas de los diferentes links */}
+                        <FooterLinkTitle>About Us</FooterLinkTitle>
+                        <FooterLink to='/sign-in'>Press</FooterLink>
+                        <FooterLink to='/sign-in'>Jobs</FooterLink>
+                        <FooterLink to='/sign-in'>Contact</FooterLink>
+                    </FooterLinkItems>
+                    <FooterLinkItems>
+                        <FooterLinkTitle>Policy</FooterLinkTitle>
+                        <FooterLink to='/sign-in'>Company</FooterLink>
+                        <FooterLink to='/sign-in'>Ensurance Quality</FooterLink>
+                        <FooterLink to='/sign-in'>Cookies</FooterLink>
+                    </FooterLinkItems>
+                </FooterLinksWrapper>
+            </FooterLinksContainer>
+            <SocialMedia>
+        <SocialMediaWrap>
+          <SocialLogo to='/'>
+            <SocialIcon />
+            H-TRACE
+          </SocialLogo>
+          <WebsiteRights>H-TRACE © 2019</WebsiteRights>
+          <SocialIcons>
+            <SocialIconLink href='/' target='_blank' aria-label='Facebook'>
+              <FaFacebook />
+            </SocialIconLink>
+            <SocialIconLink href='/' target='_blank' aria-label='Instagram'>
+              <FaInstagram />
+            </SocialIconLink>
+            <SocialIconLink
+              href={
+                '//www.youtube.com'
+              }
+              rel='noopener noreferrer'
+              target='_blank'
+              aria-label='Youtube'
+            >
+              <FaYoutube />
+            </SocialIconLink>
+            <SocialIconLink href='/' target='_blank' aria-label='Twitter'>
+              <FaTwitter />
+            </SocialIconLink>
+            <SocialIconLink href='/' target='_blank' aria-label='LinkedIn'>
+              <FaLinkedin />
+            </SocialIconLink>
+          </SocialIcons>
+        </SocialMediaWrap>
+      </SocialMedia>
         </FooterContainer>
     )
 }
