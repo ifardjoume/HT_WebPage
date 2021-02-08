031adf5780641ad8bda8705c5588624cc6cbf5f7
Gonzalo Lifrieri <gonza.lifrieri@gmail.com>
2021-02-08 16:00:30 -0300

    HashRouter

--- src/App.js
+++ src/App.js
@@ -1,6 +1,6 @@
 import React,{useState,useEffect} from 'react';
 import GlobalStyle from './globalStyles';
-import { BrowserRouter as Router, Switch, Route} from 'react-router-dom';
+import { HashRouter as Router, Switch, Route} from 'react-router-dom';
 import { IntlProvider } from 'react-intl';
 import Cookies from 'js-cookie';
 import translations from './translations/index';
